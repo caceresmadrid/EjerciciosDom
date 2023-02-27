343079c24856f35961b9d35cc5fc04924e742491
José Gilberto Cáceres Madrid <caceresmadrid@hotmail.com>
2023-02-27 15:38:39 -0600

    Repositorio 27/2/2023 15:38 ejercicio alarma

--- JS/index_dom.js
+++ JS/index_dom.js
@@ -1,8 +1,9 @@
 import hamburguerMenu from "./menu_hanburguesa.js";
-import { relojDigital, Alarma } from "./reloj.js";
+import { relojDigital, alarma } from "./reloj.js";
 
 const d=document;
 d.addEventListener("DOMContentLoaded",e=>{
     hamburguerMenu(".panel-btn",".panel",".menu a");
     relojDigital("#reloj","#activar-reloj", "#desactivar-reloj");
+    alarma("/EjerciciosDom/assets/alarma.mp3","#activar-alarma", "#desactivar-alarma");
 })
--- JS/reloj.js
+++ JS/reloj.js
@@ -22,7 +22,27 @@ export  function relojDigital(reloj, btnPlay, btnStop)
 
     })
 }
-export function Alarma()
+export function alarma(sonido, btnPlay, btnStop)
 {
+    let alarmaTempo;
+    const $alarm = d.createElement("audio");
+    $alarm.src=sonido;
+    d.addEventListener("click", e=>
+    {
+        if(e.target.matches(btnPlay))
+        {
+           alarmaTempo= setTimeout(() => {
+                $alarm.play();
+                e.target.disabled = true;
+            }, 2000);
+        }
+        if(e.target.matches(btnStop))
+        {
+            clearTimeout(alarmaTempo);
+            $alarm.pause();
+            $alarm.currentTime=0;
+            d.querySelector(btnPlay).disabled= false;
 
+        }
+    })
 }
